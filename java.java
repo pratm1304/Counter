b4ede83606fab290dcdad50d847863008113636f
pratm1304 <121380335+pratm1304@users.noreply.github.com>
2024-04-17 18:31:01 +0530

    Delete java.java

--- java.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class java{
-    public static void main(String args[]){
-       Scanner sc = new Scanner(System.in);
-       int x =sc.nextInt();
-       System.out.println(2*x);  
-    }
-}
